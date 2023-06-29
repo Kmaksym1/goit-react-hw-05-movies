90bb27524d1c48b9a434af7c41946b9179b6cd46
Maksym Kravchenko <kmaksym@gmail.com>
2023-06-29 09:26:25 -0700

    06/29 go back styled

--- src/components/movies/backLink.jsx
+++ src/components/movies/backLink.jsx
@@ -6,19 +6,21 @@ const StyledLink = styled(Link)`
     border-radius: 15px;
     margin-top: 10px;
     margin-bottom: 10px;
-    width: 227px;
-    height: 35px;
+    width: 200px;
+    height: 30px;
     display: inline-flex;
     align-items: center;
+    font-size: 30px;
     gap: 4px;
     padding: 8px 0;
     color: black;
     text-decoration: none;
-    font-weight: 500;
+    font-weight: 350;
     text-transform: uppercase;
     border: 1px solid #ccc;
     transition: background-color 0.2s ease-in-out;
     justify-content: center;
+    box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.1);
 
   &:hover {
     color: #009cf6;
@@ -31,7 +33,7 @@ const StyledLink = styled(Link)`
 export const BackLink = ({ backLinkHref, children }) => {
   return (
     <StyledLink to={backLinkHref.current}>
-      <HiArrowLeft size="40" />
+      <HiArrowLeft size="30" />
       {children}
     </StyledLink>
   );
